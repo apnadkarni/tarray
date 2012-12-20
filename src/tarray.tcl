34ac4d2ca515bc5f783a7b4359954009044a969a
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2012-12-20 19:34:44 +0530

    Make an ensemble

--- src/tarray.tcl
+++ src/tarray.tcl
@@ -1,4 +1,4 @@
-proc tarray::tupledelete {tab low {high ""}} {
+proc tarray::tabledelete {tab low {high ""}} {
     if {$high eq ""} {
         set high $low
     }
@@ -9,3 +9,28 @@ proc tarray::tupledelete {tab low {high ""}} {
 
     return $tab
 }
+
+namespace eval tarray {
+
+    namespace ensemble create -command tarray -map {
+        create create
+        delete delete
+        fill fill
+        get get
+        index index
+        list list
+        range range
+        size size
+        search search
+        sort sort
+        type type
+    }
+
+    namespace ensemble create -command ttable -map {
+        delete tabledelete
+        fill tablefill
+    }
+
+    namespace export tarray ttable
+}
+
