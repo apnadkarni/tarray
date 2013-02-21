203457559cb9a19f6d3db1e6855433db34935b70
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2013-02-21 16:46:50 +0530

    Get started on test suite

---
+++ src/build.tcl
@@ -0,0 +1,13 @@
+# Simple Tcl script to build tarray
+# Set up the appropriate environment and specify the corresponding -target
+# option. No checks are made that the build env and target match
+
+package require critcl 3.1
+package require critcl::app
+package require platform
+
+set buildarea [file normalize [file join [pwd] .. build]]
+
+# Note argv will override -target, -pkg and -libdir options if specified
+critcl::app::main [list -pkg -libdir [file join $buildarea lib] -includedir [file join $buildarea include] -cache [file join $buildarea cache] {*}$argv tarray tarray.critcl]
+
