503f9037e7f15cabe4a94e4a6e309d073d5a0337
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2013-02-21 16:46:50 +0530

    Get started on test suite

---
+++ doc/makedocs.tcl
@@ -0,0 +1,20 @@
+# Simple script to build docs
+
+set dtplite [file join [file dirname [info nameofexecutable]] dtplite.tcl]
+if {! [file exists $dtplite]} {
+    set dtplite [auto_execok dtplite.tcl]
+    if {! [file exists $dtplite]} {
+        set dtplite c:/tcl/apps/dtplite.tcl
+    }
+    if {! [file exists $dtplite]} {
+        error "dtplite not found."
+    }
+}
+
+# As an aside, note that dtplite does not like output dir to be called
+# just html. You will get strange permission denied errors.
+lassign $argv output_dir
+if {$output_dir eq ""} {
+    set output_dir [file join [file dirname [info script]] output]
+}
+exec [info nameofexecutable] $dtplite -o $output_dir html [file dirname [ info script]]
