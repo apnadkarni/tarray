536b05b865103fe11e14d2496f6e41dbfd2e608b
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2013-03-17 14:30:20 +0530

    Added critcl configuration for tarray

--- src/build.tcl
+++ src/build.tcl
@@ -1,6 +1,8 @@
 # Simple Tcl script to build tarray
 # Set up the appropriate environment and specify the corresponding -target
-# option. No checks are made that the build env and target match
+# (should be one of dev32, dev64, release32 or release64)
+# No checks are made that the build env and target match
+
 
 package require critcl 3.1
 package require critcl::app
@@ -9,5 +11,5 @@ package require platform
 set buildarea [file normalize [file join [pwd] .. build]]
 
 # Note argv will override -target, -pkg and -libdir options if specified
-critcl::app::main [list -pkg -libdir [file join $buildarea lib] -includedir [file join $buildarea include] -cache [file join $buildarea cache] {*}$argv tarray tarray.critcl]
+critcl::app::main [list -pkg -libdir [file join $buildarea lib] -includedir [file join $buildarea include] -cache [file join $buildarea cache] -config tarray.cfg -keep {*}$argv tarray tarray.critcl]
 
