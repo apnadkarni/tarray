38f118744bc0136412925c692729605310328302
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-08-04 09:06:21 +0530

    Added tas hash table

--- src/tastring.c
+++ src/tastring.c
@@ -15,6 +15,7 @@
 #define TAS_ALLOC malloc
 #define TAS_FREE  free
 
+
 /* Note len does not include trailing null and unlike Tcl_Obj's
    returned ref count is 1 on allocation */
 tas_t *tas_alloc(char *s, int len)
@@ -81,4 +82,70 @@ int tas_compare(tas_t *a, tas_t *b, int nocase)
 {
     TA_ASSERT(a->nrefs > 0 && b->nrefs > 0);
     return ta_utf8_compare(a->s, b->s, nocase);
+}
+
+
+/* The descriptor for our custom hash */
+static void tas_hash_free(Tcl_HashEntry *he);
+static unsigned int tas_hash_compute(Tcl_HashTable *phashtab, void *pkey);
+static int tas_hash_compare(void *pkey, Tcl_HashEntry *he);
+static Tcl_HashEntry *tas_hash_alloc(Tcl_HashTable *phashtab, void *pkey);
+
+const Tcl_HashKeyType tas_hash_type = {
+    TCL_HASH_KEY_TYPE_VERSION,	/* version */
+    0,				/* flags */
+    tas_hash_compute,		/* hashKeyProc */
+    tas_hash_compare,		/* compareKeysProc */
+    tas_hash_alloc,		/* allocEntryProc */
+    tas_hash_free		/* freeEntryProc */
+};
+
+void tas_hash_table_init(Tcl_HashTable *phashtab)
+{
+    Tcl_InitCustomHashTable(phashtab, TCL_CUSTOM_PTR_KEYS, &tas_hash_type);
+}
+
+static Tcl_HashEntry *tas_hash_alloc(Tcl_HashTable *phashtab, void *pkey)
+{
+    Tcl_HashEntry *he = ckalloc(sizeof(Tcl_HashEntry));
+    he->key.oneWordValue = tas_ref(pkey);
+    he->clientData = NULL;
+    return he;
+}
+
+static int tas_hash_compare(void *pkey, Tcl_HashEntry *he)
+{
+    tas_t *ptas1 = pkey;
+    tas_t *ptas2 = (tas_t *) he->key.oneWordValue;
+
+    register const char *p1, *p2;
+    register int l1, l2;
+
+    if (ptas1 == ptas2)
+        return 1;
+
+    return tas_equal(ptas1, ptas2, 0);
+}
+
+static void tas_hash_free(Tcl_HashEntry *he)
+{
+    tas_unref((tas_t *) he->key.oneWordValue);
+    ckfree(he);
+}
+
+static unsigned int tas_hash_compute(Tcl_HashTable *phashtab, void *pkey)
+{
+    const char *s = ((tas_t *)pkey)->s;
+    const unsigned char *string = pkey;
+    unsigned int result;
+    unsigned char c;
+
+    /* Exact copy of Tcl's string hash */
+    if ((result = *string) != 0) {
+	while ((c = *++string) != 0) {
+	    result += (result << 3) + c;
+	}
+    }
+
+    return result;
 }
