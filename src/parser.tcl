2c9690ae43d94f5bfecd0bc78a3d74b4785ec70b
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-06-11 16:20:46 +0530

    Very basic Selector parsing working

---
+++ src/parser.tcl
@@ -0,0 +1,152 @@
+package require pt::pgen
+package require pt::ast
+package require pt::util
+package require fileutil
+# Next line because the generated code has a return which
+# causes script to exit if not caught
+switch [catch {
+    eval [pt::pgen peg [fileutil::cat selector.peg] oo -class ::tarray::Selector]
+} msg opts] {
+    0 - 2 {}
+    default {
+        return -options $opts $msg
+    }
+}
+
+proc prast {s} {
+    set ast [parser parset $s]
+    tarray::selector::print $s $ast
+}
+tarray::Selector create parser
+
+namespace eval tarray::selector {
+    namespace export print
+    namespace ensemble create
+}
+
+proc tarray::selector::Print {s ast} {
+    set children [lassign $ast type start end]
+    set result   [list [list <$type> :: $start $end [string range $s $start $end]]]
+
+    # The arguments are already processed for printing
+    foreach c $children {
+	foreach line $c {
+	    lappend result "    $line"
+	}
+    }
+    return $result
+}
+
+
+proc tarray::selector::print {s ast} {
+    return [join [pt::ast::bottomup [list [namespace current]::Print $s] $ast] \n]    
+}
+
+namespace eval tarray::selector::topdown {}
+
+proc tarray::selector::topdown::compute {expr} {
+    set calc [Calculator new ::parser]
+    try {
+        uplevel 1 [list $calc compute $expr]
+    } finally {
+        $calc destroy
+    }
+}
+
+oo::class create tarray::selector::topdown::Calculator {
+    variable Parser Expr FrameLevel Asts
+    constructor {parser} {
+        set Parser $parser
+    }
+
+    method compute {expr} {
+        set Expr $expr
+        if {[info exists Asts($expr)]} {
+            set ast $Asts($expr)
+        } else {
+            set ast [$Parser parset $expr]
+            set Asts($expr) $ast
+        }
+        set FrameLevel "#[expr {[info level]-1}]"
+        return [my {*}$ast]
+    }
+
+    method Selector {from to firstarg args} {
+        # firstarg broken out as separate argument because it is always required
+
+        set result [my {*}$firstarg]
+        foreach arg $args {
+            if {$result} break; # Shortcut evaluation
+            set result [my {*}$arg]
+        }
+        return [expr {!!$result}]
+    }
+
+    method AndTerm {from to firstarg args} {
+        # firstarg broken out as separate argument because it is always required
+
+        set result [my {*}$firstarg]
+        foreach arg $args {
+            if {! $result} break; # Shortcut evaluation
+            set result [my {*}$arg]
+        }
+        return [expr {!!$result}]
+    }
+
+    method BoolTerm {from to firstarg args} {
+        set result [my {*}$firstarg]
+        if {[llength $args]} {
+            lassign $args relop baseterm
+            set baseterm [my {*}$baseterm]
+            # Instead of defining a method for each operator, just
+            # pick it out from the child node
+            set op [string range $Expr {*}[lrange $relop 1 2]]
+            set result [switch -exact -- $op {
+                == - != - <= - >= - < - > {
+                    tcl::mathop::$op $result $baseterm
+                }            
+
+                =^ { string equal -nocase $baseterm $result }
+                !^ { string compare -nocase $baseterm $result }
+
+                =~ { regexp -- $baseterm $result }
+                !~ { expr {![regexp -- $baseterm $result]} }
+                =^~ { regexp -nocase -- $baseterm $result }
+                !^~ { expr {![regexp -nocase -- $baseterm $result]} }
+
+                =* { string match $baseterm $result }
+                !* { expr {![string match $baseterm $result]} }
+                =^* { string match -nocase $baseterm $result }
+                !^* { expr {![string match -nocase $baseterm $result]} }
+
+                default { error "Invalid operator $op" }
+            }]
+        }
+        return [expr {!!$result}]
+    }
+
+    method BaseTerm {from to args} {
+        set val [my {*}[lindex $args end]]
+        if {[llength $args] == 1} {
+            return $val
+        }
+        if {[lindex [lindex $args 0] 0] ne "NotOp"} {
+            error "Internal error in compiler: Expected NotOp, got [lindex [lindex $args 0] 0]"
+        }
+        return [expr {! $val}]
+    }
+
+    method RealNumber {from to} {
+        return [string range $Expr $from $to]
+    }
+
+    method StringLiteral {from to} {
+        # Need to adjust by one char to remove enclosing quotes
+        return [string range $Expr [incr from] [incr to -1]]
+    }
+
+    method Var {from to} {
+        upvar $FrameLevel [string range $Expr $from $to] val
+        return $val
+    }
+}
