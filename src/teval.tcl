c29cd9db489ab1a929a6b6c416d81bd8923f3742
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-06-15 16:13:26 +0530

    Implemented arithmetic and logic operators

--- src/teval.tcl
+++ src/teval.tcl
@@ -70,21 +70,60 @@ oo::class create tarray::TEvalCompiler {
     forward Statement my _child
 
     method Assignment {from to lvalue op expr} {
-        return "set [my {*}$lvalue] [my {*}$expr]"
+        lassign [my {*}$lvalue] name index_or_range
+        set rvalue [my {*}$expr]
+        if {[llength $index_or_range] == 0} {
+            return "set [list $name] $rvalue"
+        } else {
+            return "tarray::column::vfill [list $name] $rvalue $index_or_range"
+        }
     }
 
-    forward LValue my _child
+    method LValue {from to ident {index_or_range {}}} {
+        set name [my {*}$ident]
+        if {[llength $index_or_range] == 0} {
+            return [list $name]
+        }
+        return [list $name {*}[my {*}$index_or_range]]
+    }
+
     forward Identifier my _extract
-    forward Expression my _child
-    forward LogicalOrExpr my _child
-    forward LogicalAndExpr my _child
-    forward BitOrExpr my _child
-    forward BitXorExpr my _child
-    forward BitAndExpr my _child
-    forward EqExpr my _child
-    forward AddExpr my _child
-    forward MulExpr my _child
-    forward UnaryExpr my _child
+    method Expression {from to child} {
+        return "\[expr {[my {*}$child]}\]"
+    }
+    
+    method _join_specific_operator {op from to args} {
+        join [lmap child $args {
+            my {*}$child
+        }] $op
+    }
+
+    method _join_operator {from to first_child args} {
+        if {[llength $args] == 0} {
+            return [my {*}$first_child]
+        } else {
+            return [my _join_specific_operator [my {*}[lindex $args 0]] $from $to $first_child {*}[lrange $args 1 end]]
+        }
+    }
+
+    forward LogicalOrExpr my _join_specific_operator  ||
+    forward LogicalAndExpr my _join_specific_operator &&
+    forward BitOrExpr my _join_specific_operator      |
+    forward BitXorExpr my _join_specific_operator     ^
+    forward BitAndExpr my _join_specific_operator     &
+
+    forward RelExpr my _join_operator
+    forward AddExpr my _join_operator
+    forward MulExpr my _join_operator
+
+    method UnaryExpr {from to args} {
+        if {[llength $args] == 1} {
+            return [my {*}[lindex $args 0]]
+        } else {
+            return "[my {*}[lindex $args 0]][my {*}[lindex $args 1]]"
+        }
+    }
+
     forward PostfixExpr my _child
     method PrimaryExpr {from to child} {
         if {[lindex $child 0] eq "Identifier"} {
@@ -95,6 +134,32 @@ oo::class create tarray::TEvalCompiler {
             return [my {*}$child]
         }
     }
+    method Index {from to {expr {}}} {
+        if {[llength $expr] == 0} {
+            return [string range $Script $from $to]
+        }
+        return [my {*}$expr]
+    }
+
+    forward Range my _child
+    method RangeLow {from to expr} {
+        return [list [{*}$expr] "end"]
+    }
+    method RangeHigh {from to expr} {
+        return [list 0 [{*}$expr]]
+    }
+    method RangeLowHigh {from to lowexpr highexpr} {
+        return [list [{*}$lowexpr] [{*}$highexpr]]
+    }
+    method RangeFull {from to} {
+        return [list 0 "end"]
+    }
+
+    forward Number my  _extract
+    forward RelOp my   _extract
+    forward UnaryOp my _extract
+    forward AddOp my   _extract
+    forward MulOp my   _extract
 }
 
 proc prast {parser s} {
