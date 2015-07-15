ac947bb17b12607d0a114ef9e875f0a4ac2c39ec
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-07-15 08:45:39 +0530

    Partial implementation of casting

--- src/tarray.h
+++ src/tarray.h
@@ -442,6 +442,9 @@ TCL_RESULT ta_get_byte_from_obj(Tcl_Interp *ip, Tcl_Obj *o, unsigned char *pb);
 TCL_RESULT ta_get_uint_from_obj(Tcl_Interp *ip, Tcl_Obj *o, unsigned int *pui);
 TCL_RESULT ta_get_int_from_obj(Tcl_Interp *ip, Tcl_Obj *o, int *pi);
 
+TCL_RESULT ta_get_wide_from_string(Tcl_Interp *ip, char *s, Tcl_WideInt *pi);
+TCL_RESULT ta_get_double_from_string(Tcl_Interp *ip, char *s, double *pi);
+
 void thdr_incr_obj_refs(thdr_t *thdr,int first,int count);
 void thdr_decr_obj_refs(thdr_t *thdr,int first,int count);
 void thdr_incr_tas_refs(thdr_t *thdr, int first, int count);
