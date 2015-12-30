9365758ba2227620b930f65a75d946b649065b4f
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-12-30 21:59:50 +0530

    Automate release file versioning

--- release.cmd
+++ release.cmd
@@ -1,4 +1,9 @@
 rmdir/s/q build
+@rem Form the file names based on the version we are building
+@for /f %%i in ('tclsh src/taversion.tcl') do set TANAME=tarray-%%i
+@for /f %%i in ('tclsh src/taversion.tcl') do set XTALNAME=xtal-%%i
+@for /f %%i in ('tclsh ui/uiversion.tcl') do set UINAME=tarray_ui-%%i
+
 cmd /c "envset x86 && cd src && tclsh build.tcl extension -target win32-ix86-cl"
 cmd /c "envset x64 && cd src && tclsh build.tcl extension -target win32-x86_64-cl"
 cmd /c "cd src && tclsh build.tcl tea"
@@ -7,10 +12,10 @@ cmd /c "envset x64 && cd xtal && tclsh build.tcl extension -target win32-x86_64-
 cmd /c "cd xtal && tclsh build.tcl tea"
 cmd /c "cd ui && tclsh build.tcl package"
 
-cd build\lib && zip -r tarray.zip tarray && move tarray.zip .. && cd ..\..
-cd build\lib && zip -r xtal.zip xtal && move xtal.zip .. && cd ..\..
-cd build\lib && zip -r tarray_ui.zip tarray_ui && move tarray_ui.zip .. && cd ..\..
+cd build\lib && zip -r tarray.zip tarray && move tarray.zip ..\%TANAME%.zip  && cd ..\..
+cd build\lib && zip -r xtal.zip xtal && move xtal.zip ..\%XTALNAME%.zip && cd ..\..
+cd build\lib && zip -r tarray_ui.zip tarray_ui && move tarray_ui.zip ..\%UINAME%.zip && cd ..\..
 
-cd build\tea && tar cvf tarray.tar tarray && gzip tarray.tar && move tarray.tar.gz .. && cd ..\..
-cd build\tea && tar cvf xtal.tar xtal && gzip xtal.tar && move xtal.tar.gz .. && cd ..\..
-cd build\lib && tar cvf tarray_ui.tar tarray_ui && gzip tarray_ui.tar && move tarray_ui.tar.gz .. && cd ..\..
+cd build\tea && tar cvf tarray.tar tarray && gzip tarray.tar && move tarray.tar.gz ..\%TANAME%.tar.gz && cd ..\..
+cd build\tea && tar cvf xtal.tar xtal && gzip xtal.tar && move xtal.tar.gz ..\%XTALNAME%.tar.gz && cd ..\..
+cd build\lib && tar cvf tarray_ui.tar tarray_ui && gzip tarray_ui.tar && move tarray_ui.tar.gz ..\%UINAME%.tar.gz && cd ..\..
--- ui/uiversion.tcl
+++ ui/uiversion.tcl
@@ -1,4 +1,8 @@
 namespace eval tarray::ui {
     proc version {} {return 0.8}
+    # Print version if this file is the main script. Used during builds.
+    if {[file tail [info script]] eq [file tail [lindex $argv0 0]]} {
+        puts [version]
+    }
 }
 return [tarray::ui::version]
