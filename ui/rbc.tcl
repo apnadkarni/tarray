7bd52e3d65ce8eca80cf1d5ab1d2227019da3b84
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-02-08 13:24:39 +0530

    Fix tovector when indices specified

--- ui/rbc.tcl
+++ ui/rbc.tcl
@@ -16,6 +16,11 @@ package require snit
     # Holds the data table being displayed
     variable _table
     
+    # Index column holding order of elements to be plotted.
+    # The graph data is expected to be in ascending x-value order.
+    # This column holds that order.
+    variable _indices
+
     # Array mapping columns to vectors names. Indexed by column name
     variable _vectors
 
@@ -66,19 +71,24 @@ package require snit
         if {$cname ni [tarray::table cnames $_table]} {
             error "Table does not contain column named $cname"
         }
-        set ctype [tarray::column type [tarray::table column $_table $cname]]
+        set tcol [tarray::table column $_table $cname]
+        set ctype [tarray::column type $tcol]
         if {$ctype in {any string}} {
             # Create a index vector that maps to the non-numeric values
             set _vectors($cname) [rbc::vector create #auto]
-            $_vectors($cname) seq 0 [expr {[tarray::table size $_table]-1}]
+            set n [tarray::column size $tcol]
+            set _indices [tarray::column series $n]
+            $_vectors($cname) seq 0 [expr {$n-1}]
             # Add the callback to map index to value
             $win axis configure x -subdivisions 1 -command [list $self _map_xvalue]
         } else {
+            set _indices [tarray::column sort -indices $tcol]
             $self _vectorify $cname
             # Remove any callback that mapped indexes to string values
             $win axis configure x -subdivisions 2 -command {}
         }
         set options(-xcolumn) $cname
+        return
     }
 
     method _map_xvalue {w val} {
@@ -94,7 +104,7 @@ package require snit
         if {[info exists _vectors($cname)]} return
         set _vectors($cname) [::rbc::vector create #auto]
         if {[catch {
-            tarray::rbc::tovector $_vectors($cname) [tarray::table::column $_table $cname]
+            tarray::rbc::tovector $_vectors($cname) [tarray::table::column $_table $cname] $_indices
         } result ropts]} {
             rbc::vector destroy $_vectors($cname)
             unset _vectors($cname)
@@ -112,5 +122,6 @@ set tab [tarray::table create {
 } {
     {China 100 1350000000}
     {Vatican 200 850}
+    {India 300 1200000000}
 }]
 destroy .win
