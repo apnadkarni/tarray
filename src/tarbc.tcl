29a710764e1515c03dba39f3b3de4e66b87bf50f
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-03-29 21:53:15 +0530

    Implement to/fromvector at script level if rbc stubs not available

--- src/tarbc.tcl
+++ src/tarbc.tcl
@@ -1,11 +1,12 @@
 #
-# Copyright (c) 2017 Ashok P. Nadkarni
+# Copyright (c) 2017-2018 Ashok P. Nadkarni
 # All rights reserved.
 #
 # See the file LICENSE for license
 #
 
 namespace eval tarray::rbc {
+    # TBD - document tovector and fromvector
     # Define our rbc vector procedures to initialize rbc and then call
     # the *redefined* commands of the same name.
     foreach name {fromvector tovector} {
@@ -14,9 +15,53 @@ namespace eval tarray::rbc {
 
     # Initializes rbc and defines the real commands
     proc init {} {
-        # The uplevel is required to define C commands in global context
-        uplevel #0 tarray::rbc::init_stubs
         proc init {} {}
+        
+        # The uplevel is required to define C commands in global context
+        if {[catch {uplevel #0 tarray::rbc::init_stubs}]} {
+            # Perhaps we are running against RBC without stubs
+            # Then define procedures the slow way
+            
+            proc fromvector {vec args} {
+                if {[llength $args] == 0} {
+                    set first 0
+                    set last end
+                } elseif {[llength $args] == 2} {
+                    lassign $args first last
+                } else {
+                    error "wrong #args: should be \"fromvector VEC ?FIRST LAST?\""
+                }
+                # $vec has to be resolved in context of caller
+                set vals [uplevel 1 [list [$vec range $first $last]]]
+                return [tarray::column create double $vals]
+            }
+
+            proc tovector {vec col args} {
+                if {[llength $args] == 0} {
+                    set vals [tarray::column range -list $col 0 end]
+                } elseif {[llength $args] == 1} {
+                    set vals [tarray::column get -list $col [lindex $args 0]]
+                } else {
+                    error "wrong #args: should be \"tovector VEC COLUMN ?INDICES?\""
+                }
+                    
+                # Note $vec has to be resolved in context of caller, hence the uplevels
+                set qual_vec [uplevel 1 [list namespace which -command $vec]]
+                if {$qual_vec eq "" || $qual_vec ni [rbc::vector names]} {
+                    uplevel 1 [list rbc::vector create $qual_vec]
+                    try {
+                        $qual_vec append $vals
+                    } trap {} {msg ropts} {
+                        vector destroy $qual_vec
+                        return $ropts $msg
+                    }
+                } else {
+                    $qual_vec length 0
+                    $qual_vec append $vals
+                }
+                return $vec;    # Return same name as originally passed
+            }
+        }
     }
 }
 
