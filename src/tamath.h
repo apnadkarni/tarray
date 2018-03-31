e316f644c612ae9ea8e84c9e31c032500f640bbe
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-03-31 15:48:44 +0530

    Add integer overflow checking functions

---
+++ src/tamath.h
@@ -0,0 +1,191 @@
+#ifndef TAMATH_H
+#define TAMATH_H
+
+/* 
+ * Functions for overflow detection in integer operations.
+ * Only operations used in tarray are defined.
+ * References:
+ *   https://wiki.sei.cmu.edu/confluence/display/c/INT32-C.+Ensure+that+operations+on+signed+integers+do+not+result+in+overflow
+ *   https://wiki.sei.cmu.edu/confluence/display/c/INT30-C.+Ensure+that+unsigned+integer+operations+do+not+wrap
+ *   https://gcc.gnu.org/onlinedocs/gcc/Integer-Overflow-Builtins.html
+ */
+
+#ifdef __FAST_MATH__
+# error -ffast-math option of GCC will cause incorrect calculations.
+#endif
+
+/* Visual C++ prior to Visual Studio 2010 do not have stdint */
+#if defined(_MSC_VER) && _MSC_VER < 1700
+#include "ms_stdint.h"
+#else
+#include <stdint.h>
+#endif
+
+#ifndef TA_INLINE
+# ifdef _MSC_VER
+#  define TA_INLINE __inline  /* Because VC++ 6 only accepts "inline" in C++  */
+# elif __GNUC__ && !__GNUC_STDC_INLINE__
+#  define TA_INLINE extern inline
+# else
+#  define TA_INLINE inline
+# endif
+#endif
+
+
+#if __GNUC__ >= 5
+
+/****************************************************************
+ * gcc has direct built-ins for integer overflow detection 
+ ****************************************************************/
+
+/* Generic gcc built-in */
+#define OVF_ADD_FN(type_) \
+    TA_INLINE int ovf_add_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) {
+    return __builtin_add_overflow(a, b, presult);
+}
+#define OVF_ADDU_FN OVF_ADD_FN
+
+OVF_ADD_FN(int8)
+OVF_ADDU_FN(uint8)
+
+/* For 32-bit use exact built-ins */
+#define ovf_add_int32  __builtin_sadd_overflow
+#define ovf_add_uint32 __builtin_uadd_overflow
+/* For 64-bit assumes ll is 64 bits */
+#define ovf_add_int64  __builtin_saddll_overflow
+
+/* Generic gcc built-in */
+#define OVF_SUB_FN(type_) \
+    TA_INLINE int ovf_sub_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) {
+    return __builtin_sub_overflow(a, b, presult);
+}
+#define OVF_SUBU_FN OVF_SUB_FN
+
+OVF_SUB_FN(int8)
+OVF_SUBU_FN(uint8)
+
+/* For 32-bit use exact built-ins */
+#define ovf_sub_int32  __builtin_ssub_overflow
+#define ovf_sub_uint32 __builtin_usub_overflow
+/* For 64-bit assumes ll is 64 bits */
+#define ovf_sub_int64  __builtin_ssubll_overflow
+
+/* Generic gcc built-in */
+#define OVF_MUL_FN(type_) \
+    TA_INLINE int ovf_mul_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) {
+    return __builtin_mul_overflow(a, b, presult);
+}
+#define OVF_MULU_FN OVF_MUL_FN
+
+OVF_MUL_FN(int8)
+OVF_MULU_FN(uint8)
+
+/* For 32-bit use exact built-ins */
+#define ovf_mul_int32  __builtin_smul_overflow
+#define ovf_mul_uint32 __builtin_umul_overflow
+/* For 64-bit assumes ll is 64 bits */
+#define ovf_mul_int64  __builtin_smulll_overflow
+
+#else
+
+/****************************************************************
+ * Not gcc - define portable versions
+ ****************************************************************/
+
+#define OVF_ADD_FN(type_, min_, max_) \
+    TA_INLINE int ovf_add_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        int64_t result = (int64_t) a + (int64_t) b;                     \
+        *presult = (type_ ## _t) result;                                \
+        return (result > max_ || result < min_);                        \
+    }
+
+#define OVF_ADDU_FN(type_) \
+    TA_INLINE int ovf_add_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        *presult = a + b;                                     \
+        return *presult < a;                                              \
+    }
+
+OVF_ADD_FN(int8, INT8_MIN, INT8_MAX)
+OVF_ADDU_FN(uint8)
+OVF_ADD_FN(int32, INT32_MIN, INT32_MAX)
+OVF_ADDU_FN(uint32)
+
+TA_INLINE int ovf_add_int64(int64_t a, int64_t b, int64_t *presult) {
+    *presult = a + b;
+    return ((b > 0 && a > (INT64_MAX - b)) ||
+            (b < 0) && a < (INT64_MIN - b));
+}
+
+#define OVF_SUB_FN(type_, min_, max_) \
+    TA_INLINE int ovf_sub_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        int64_t result = (int64_t) a - (int64_t) b;                     \
+        *presult = (type_ ## _t) result;                                \
+        return (result > max_ || result < min_);                        \
+    }
+
+#define OVF_SUBU_FN(type_) \
+    TA_INLINE int ovf_sub_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        *presult = a - b;                                              \
+        return (a < b) ;                                                \
+    }
+
+OVF_SUB_FN(int8, INT8_MIN, INT8_MAX)
+OVF_SUBU_FN(uint8)
+OVF_SUB_FN(int32, INT32_MIN, INT32_MAX)
+OVF_SUBU_FN(uint32)
+
+TA_INLINE int ovf_sub_int64(int64_t a, int64_t b, int64_t *presult) {
+    *presult = a - b;
+    return ((b > 0 && a < (INT64_MIN + b)) ||
+            (b < 0) && a > (INT64_MAX + b));
+}
+
+#define OVF_MUL_FN(type_, min_, max_) \
+    TA_INLINE int ovf_mul_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        int64_t result = (int64_t) a * (int64_t) b;                     \
+        *presult = (type_ ## _t) result;                                \
+        return (result > max_ || result < min_);                        \
+    }
+
+#define OVF_MULU_FN(type_, max_) \
+    TA_INLINE int ovf_mul_ ## type_ (type_ ## _t a, type_ ## _t b, type_ ## _t *presult) \
+    {                                                                   \
+        int64_t result = (int64_t) a * (int64_t) b;                     \
+        *presult = (type_ ## _t) result;                                \
+        return (result > max_);                                         \
+    }
+
+OVF_MUL_FN(int8, INT8_MIN, INT8_MAX)
+OVF_MULU_FN(uint8, UINT8_MAX)
+OVF_MUL_FN(int32, INT32_MIN, INT32_MAX)
+OVF_MULU_FN(uint32, UINT32_MAX)
+
+int ovf_mul_int64_impl(int64_t a, int64_t b, int64_t *presult);
+#if defined(_MSC_VER) && defined(_M_AMD64)
+
+#include <intrin.h>
+#pragma intrinsic(_mul128)
+
+TA_INLINE int ovf_mul_int64(int64_t a, int64_t b, int64_t *presult) {
+    __int64 high;
+    *presult = _mul128(a, b, &high);
+    return (high != 0);
+}
+
+#else
+
+/* Too long to be inline - defined in tamath.c */
+#define ovf_mul_int64 ovf_mul_int64_impl
+
+#endif
+
+
+#endif /* __GNUC__ >= 5 */
+
+
+#endif
